701abc3786e137a8ba87ea58cd0d55513bc12a07
0x002A <kevin@0x002a.com>
2019-02-20 00:41:01 +0100

    [#1069] some changes and tests added for snapshot operator (cherry picked from commit b3356a6)

---
+++ gradoop-flink/src/test/java/org/gradoop/flink/model/impl/operators/tpgm/snapshot/functions/Extractor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2014 - 2018 Leipzig University (Database Research Group)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradoop.flink.model.impl.operators.tpgm.snapshot.functions;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.gradoop.common.model.api.entities.EPGMElement;
+import org.gradoop.flink.model.GradoopFlinkTestBase;
+import org.gradoop.flink.model.api.functions.TimeIntervalExtractor;
+
+public class Extractor<E extends EPGMElement>
+  extends GradoopFlinkTestBase implements TimeIntervalExtractor<E> {
+
+  /**
+   * Serial.
+   */
+  private static final long serialVersionUID = 42L;
+
+  @Override
+  public Tuple2<Long, Long> map(E element) throws Exception {
+    return extractTime(element);
+  }
+}
